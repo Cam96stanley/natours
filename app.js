3a57f1694882319703eb6e81e3487c97924cdd6e
Cameron Stanley <cam96stanley@gmail.com>
2025-11-05 00:19:55 -0500

    created basic tour model and routes

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MiddleWares
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
