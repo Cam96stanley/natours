b0fc35c6547be9d56487f7102675535c1b22298f
Cameron Stanley <cam96stanley@gmail.com>
2025-11-05 23:50:16 -0500

    created error handling functions

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLED EXCEPTION! SHUTTING DOWN...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -14,6 +20,14 @@ mongoose.connect(DB).then(console.log('DB connection successfull'));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
