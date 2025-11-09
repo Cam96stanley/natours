f5cd6c4202e1b03756dd1e134c56554d59cbc12e
cam96stanley <cam96stanley@gmail.com>
2025-11-09 18:53:07 -0500

    created file uploading functionality

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -13117,12 +13117,11 @@ if (logoutButton) logoutButton.addEventListener('click', _login.logout);
 if (userDataForm) {
   userDataForm.addEventListener('submit', function (e) {
     e.preventDefault();
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    (0, _updateSettings.updateSettings)({
-      name: name,
-      email: email
-    }, 'data');
+    var form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    (0, _updateSettings.updateSettings)(form, 'data');
   });
 }
 if (userPasswordForm) {
@@ -13183,7 +13182,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56852" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51142" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
